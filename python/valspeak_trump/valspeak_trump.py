0a55a14c87d1d8a351226e0fd831e01d50ba2a3b
Colin Millerchip <colin@millerchip.org>
2020-03-02 19:12:01 +0000

    Completed API integration, but switched from valspeak to gungan, so need to sort out naming of folder & file

--- python/valspeak_trump/valspeak_trump.py
+++ python/valspeak_trump/valspeak_trump.py
@@ -3,13 +3,12 @@
 # set debug flag to True, to run the program but not actually create the tweet
 debug = True
 
-
 # https://stackoverflow.com/questions/3878555/how-to-replace-repeated-instances-of-a-character-with-a-single-instance-of-that
 # https://docs.python.org/3/library/re.html#re.sub
 # for getting rid of duplicate characters (specifically, spaces)
 import re
 
-# Might need this to parse the response from the valspeak API
+# Might need this to parse the response from the conversion API
 import json
 
 # https://medium.freecodecamp.org/creating-a-twitter-bot-in-python-with-tweepy-ac524157a607
@@ -26,7 +25,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 user = api.me()
 
-# FIND FIRST TWEET NOT OF ZERO LENGTH (ITERATE THROUGH LAST 20, SO CURRENTLY WILL FAIL IT NO TWEET IS NON-ZERO LENGTH)
+# FIND FIRST TWEET NOT OF ZERO LENGTH (ITERATE THROUGH AT MOST THE LAST 20, SO CURRENTLY WILL FAIL IT NO TWEET IS NON-ZERO LENGTH)
 # https://twitter.com/realDonaldTrump
 # From https://stackoverflow.com/questions/27900451/convert-tweepy-status-object-into-json
 # OTHER SITES, MIGHT BE USEFUL FUTURE REFERENCES
@@ -54,13 +53,10 @@ if debug:
     print("tweet = " + original_tweet)
     print("length = " + str(tweet_length))
 
+# Convert the tweet using public API at https://api.funtranslations.com/
+# Parse response from resultant JSON, and extract translation into variable 'converted_tweet'
 
-# Convert the tweet into valspeak, using public API at https://funtranslations.com/api/valspeak
-# Parse response from resultant JSON, and extract translation into variable 'valspeak_tweet'
-
-# Call API, but need to convert text
-# https://api.funtranslations.com/translate/valspeak.json?text=Good%20Morning.%20Come%20on%20man%2C%20just%20saying%21
-
+# Call API, but need to encode the text
 # https://stackoverflow.com/questions/1695183/how-to-percent-encode-url-parameters-in-python
 # https://docs.python.org/3/library/urllib.html#urllib.quote
 from urllib.parse import quote
@@ -75,10 +71,9 @@ encoded_tweet = quote(original_tweet, safe='')
 import requests 
 
 # api-endpoint 
-# https://funtranslations.com/api/valspeak
-URL = "https://api.funtranslations.com/translate/valspeak.json"
-
-# I tested that the API is working, but I suspect that Trump's tweets as so grammatically challenging that the API is struggling to parse them!
+# https://api.funtranslations.com/
+# I tried several APIs, but many don't work well with short and grammatically challenging tweets. However, this one does:
+URL = "https://api.funtranslations.com/translate/gungan.json"
 
 # defining a params dict for the parameters to be sent to the API 
 PARAMS = {'text':encoded_tweet} 
@@ -89,57 +84,43 @@ r = requests.get(url = URL, params = PARAMS)
 # extracting data in json format 
 data = r.json()
 
-print("\n")
+print("\ndata:")
 print(data)
-print("\n")
 
-# from https://funtranslations.com/api/valspeak, this is definitely the right field, but what I'm currently getting back from the API doesn't look like it's being parsed. However, I only get 5 API calls an hour on the free account, so will have to test slowly! :o/
+# I only get 5 API calls an hour on the free account, so will have to test slowly! :o/
 # TODO should also check data['success']['total'], which should be 1 (numeric value)
-valspeak_tweet = unquote(data['contents']['translated'])
-print("\nVALSPEAK CONVERTED\n")
-print("valspeak_tweet = " + valspeak_tweet)
+converted_tweet = unquote(data['contents']['translated'])
+
+print("\nConverted tweet = " + converted_tweet)
 
 exit()
-
-print(data)
-
-exit()
-
-# Original tweet: I hope we can get Admiral @RonnyJackson4TX of Texas, who served our Country so well, into the runoff election in #TX13! Ronny is strong on Crime and Borders, GREAT for our Military and Vets, and will protect your #2A. Get out and vote for Ronny on Tuesday, March 3rd!
-
-# Valspeak returned value: I%20hope%20we%20can%20get%20Admiral%20%40RonnyJackson4TX%20of%20Texas%2C%20who%20served%20our%20Country%20so%20well%2C%20into%20the%20runoff%20election%20in%20%23TX13%21%20Ronny%20is%20strong%20on%20Crime%20and%20Borders%2C%20GREAT%20for%20our%20Military%20and%20Vets%2C%20and%20will%20protect%20your%20%232A.%20Get%20out%20and%20vote%20for%20Ronny%20on%20Tuesday%2C%20March%203rd%21
-
-
-# API returned this value:
-{'success': {'total': 1}, 'contents': {'translated': 'I%20hope%20we%20can%20get%20Admiral%20%40RonnyJackson4TX%20of%20Texas%2C%20who%20served%20our%20Country%20so%20well%2C%20into%20the%20runoff%20election%20in%20%23TX13%21%20Ronny%20is%20strong%20on%20Crime%20and%20Borders%2C%20GREAT%20for%20our%20Military%20and%20Vets%2C%20and%20will%20protect%20your%20%232A.%20Get%20out%20and%20vote%20for%20Ronny%20on%20Tuesday%2C%20March%203rd%21', 'text': 'I%20hope%20we%20can%20get%20Admiral%20%40RonnyJackson4TX%20of%20Texas%2C%20who%20served%20our%20Country%20so%20well%2C%20into%20the%20runoff%20election%20in%20%23TX13%21%20Ronny%20is%20strong%20on%20Crime%20and%20Borders%2C%20GREAT%20for%20our%20Military%20and%20Vets%2C%20and%20will%20protect%20your%20%232A.%20Get%20out%20and%20vote%20for%20Ronny%20on%20Tuesday%2C%20March%203rd%21', 'translation': 'valspeak'}}
-
 
 # Testing
 # original_tweet = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including and then"
 
 
-valspeak_tweet = "Lorem Ipsum is simply dummy text of thuh printin' and typesettin' industry. Lorem Ipsum has been thuh industry's standard dummy text ever since thuh 1500s, mostly when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, like, wow but also thuh leap into electronic typesettin', mostly remainin' essentially unchanged. It was like, ya know, popularised in thuh 1960s with thuh release of Letraset sheets containin' Lorem Ipsum passages, like, wow and more recently with desktop publishin' software like Aldus PageMaker includin' and then" 
+converted_tweet = "Lorem ipsum is simply dummy text of dha print and typesett industry. Lorem ipsum has been dha industry's standard dummy text ever since dha 1500s, when an unknown printer took a galley of type and scrambled it to maken a type specimen book. It has survived not only fife centuries, but also dha leap into electronic typesett, remain essentially unchanged. It was popularised in dha 1960s with dha release of letraset sheets contain lorem ipsum passages, and more recently with desktop publish software like aldus pagemaker includ and then" 
 
 
 # ... and now tweet, breaking into separate tweets if necessary
 # this code first used for PlatBot, which was parsing a v.short blog post, and hence could result in multiple tweets. 
-# In practice, translating to valspeak will only fractionally lengthen a tweet, so could update this to handle the fact that there would be at most
+# In practice, translation will only fractionally lengthen a tweet, so could update this to handle the fact that there would be at most
 # 2 tweets... but why bother, this works.
-if (len(valspeak_tweet)>1): 
+if (len(converted_tweet)>1): 
     # it's a non-trivial length string
-    if len(valspeak_tweet) > 280:
+    if len(converted_tweet) > 280:
         # break into segments
         i = 1
         while True:
             # Assume update will not require more than 9 tweets, hence len(str(i)) = 1
-            if len(valspeak_tweet) > 277:
+            if len(converted_tweet) > 277:
                 # truncate at a word boundary (ie, not part-way through a word)
-                last_space = valspeak_tweet[0:276].rfind(" ")
-                twt = str(i) + "/ " + valspeak_tweet[0:last_space]
-                valspeak_tweet = valspeak_tweet[(last_space+1):]
+                last_space = converted_tweet[0:276].rfind(" ")
+                twt = str(i) + "/ " + converted_tweet[0:last_space]
+                converted_tweet = converted_tweet[(last_space+1):]
             else:
-                twt = str(i) + "/ " + valspeak_tweet
-                valspeak_tweet = ""
+                twt = str(i) + "/ " + converted_tweet
+                converted_tweet = ""
             i += 1
             print ("Tweet will be this: " + twt)
             if (not debug):
@@ -147,12 +128,12 @@ if (len(valspeak_tweet)>1):
                 print("*** tweet created")
             else:
                 print("*** debug mode -- tweet not created")
-            if len(valspeak_tweet) < 1:
+            if len(converted_tweet) < 1:
                 break
     else:
-        print ("Tweet will be this: " + valspeak_tweet)
+        print ("Tweet will be this: " + converted_tweet)
         if (not debug):
-            # api.update_status (valspeak_tweet)
+            # api.update_status (converted_tweet)
             print("*** tweet created")
         else:
             print("*** debug mode -- tweet not created")
